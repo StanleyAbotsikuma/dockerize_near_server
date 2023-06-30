c4d60943dee6c9363324243aa0286ae5f9ea0625
StanleyAbotsikuma <abotsistanley0@gmail.com>
2023-06-30 19:52:40 +0000

    remove workflow

--- nears_server/near_server/settings.py
+++ nears_server/near_server/settings.py
@@ -208,8 +208,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 
-STATIC_URL = 'static/'
+
+STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR/'media/'
 MEDIA_URL = '/media/'
 # Default primary key field type
