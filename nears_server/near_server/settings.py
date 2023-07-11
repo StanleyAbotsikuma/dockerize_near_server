f82fc7a01373f12b8ed235f12a400d5d8ac1a0ed
StanleyAbotsikuma <abotsistanley0@gmail.com>
2023-07-11 06:53:03 +0000

    fixed

--- nears_server/near_server/settings.py
+++ nears_server/near_server/settings.py
@@ -200,11 +200,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATICFILES_DIRS = [
-     "/var/www/static/",
+    
     BASE_DIR / "static"
 ]
-
-STATIC_URL =BASE_DIR/ 'static/'
+STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL ='static/'
 MEDIA_ROOT = BASE_DIR/'media/'
 MEDIA_URL = '/media/'
 # Default primary key field type
